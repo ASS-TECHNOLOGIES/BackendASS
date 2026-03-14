f8798618ad2f5fcc638e0064d99cf4227432496f
Alexander Denton <100879026+alexanderdenton@users.noreply.github.com>
2026-03-14 15:37:00 +0000

    Renamed student class.

---
+++ StudyASS/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudyASS.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudyASS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private IDatabaseParser _parser;
+
+        public StudentController(IDatabaseParser databaseParser) : base()
+        {
+            _parser = databaseParser;
+        }
+
+        // GET: api/<StudentsController>
+        [HttpGet(Name = "GetStudents")]
+        public IEnumerable<IStudent> Get()
+        {
+            return _parser.GetStudents();
+        }
+    }
+}
